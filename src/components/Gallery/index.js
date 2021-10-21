50a98cf35592c733ac682ad6f9c911391d75a333
Sean <sdillon215@gmail.com>
2021-10-21 19:12:52 -0400

    hardcoded gallery comp

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from '../../assets/small/commercial/0.jpg';
+
+function Gallery() {
+    const currentCategory = {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other stuff",
+    };
+    return (
+        <section>
+            <hi>{capitalizeFirstLetter(currentCategory.name)}</hi>
+            <p>{currentCategory.name}</p>
+            <div>
+                <img
+                    src={photo}
+                    alt="Commercial Example"
+                    className="img-thumbnail mx-1"
+                />
+            </div>
+        </section>
+    );
+}
+
+export default Gallery;
